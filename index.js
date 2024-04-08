9a3aae5ff03b06b214551ea4e6bdec3e848c57bc
David Mold <davidmold@gmail.com>
2024-04-08 10:23:16 -0400

    updated number of trees and start date

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const totalTreesAtTimeKnown = 1044932.0
+const totalTreesAtTimeKnown = 1941028.0
 const treesPerHour = 18.7733
 
 module.exports = {
   countTrees: function () {
     let timenow = new Date().getTime();
-    let timeknown = new Date(2018, 6, 9).getTime()
+    let timeknown = new Date(2024, 3, 8).getTime()
     
     let treesPerTick = treesPerHour / 60.0 / 60.0 / 1000.0
     let diff = timenow - timeknown
